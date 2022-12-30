c1ac73e3bda270ebbfd60ff1c3cf0146ea84b396
dtholen <dieter.tholen@web.de>
2022-12-30 16:37:28 +0100

    tutor2

--- tutor2.py
+++ tutor2.py
@@ -1,1 +1,14 @@
 # tutor2
+# Import modules
+from pitop.pma import Button
+
+# Set up the button on port D1 of the foundation plate
+button = Button("D5")
+
+# Loop until the button is pressed
+while not button.is_pressed:
+    pass
+
+# Print to the console
+print("Button Pressed")
+  
