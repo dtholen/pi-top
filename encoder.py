693172e80c23163e41906b12297d3f533c4b3abd
dtholen <dieter.tholen@web.de>
2023-01-07 09:53:52 +0100

    update

--- encoder.py
+++ encoder.py
@@ -1,8 +1,10 @@
 # Import modules
+import sys
 from pitop import EncoderMotor, ForwardDirection  # Motor control library
 from time import sleep  # Allows us to tell the program to wait
 from pitop.miniscreen import Miniscreen
 from pitop.pma import UltrasonicSensor,Buzzer, Button, LED
+
 
 ultrasonic_sensor = UltrasonicSensor("D0")
 
@@ -16,17 +18,54 @@ setCounter = [100,100,100]
 #Move weel
 def moveWeel(speed):
   global sp,id,ultrasonoc_sensor
-  motor_r.set_target_rpm(speed)
-  motor_l.set_target_rpm(-speed)
+  motor_r.set_target_rpm(-speed)
+  motor_l.set_target_rpm(speed)
+
+def turn(speed, time):
+    print ("turn",speed, time)
+    motor_r.set_target_rpm(speed)
+    motor_l.set_target_rpm(speed)
+    sleep(time)
+    stopWeel()
+
+def collision(speed, time):
+    turn(speed,time)
+    moveWeel(-speed)
+    sleep(1)
+    stopWeel()
+  
 def stopWeel():
   motor_r.stop()
   motor_l.stop()
+  
+args=sys.argv
+for arg in args:
+  print ('Mode:',arg)
+  if (arg== "1"):
+      print ('Hello there', arg)
+  if (arg== "2"):
+      print ('--> drive forward',speed)
+      moveWeel(speed)
+      sleep(1)
+      stopWeel()
+  if (arg == "3"):
+      print ('--> drive backward',speed)
+      moveWeel(-speed)
+      sleep(1)
+      stopWeel()
+  if (arg== "4"):
+      print ('--> turn r',speed)
+      turn(speed,1.0)
+  if (arg== "5"):
+      print ('--> turn l',-speed)
+      turn(-speed,1.0)
+  if (arg== "6"):
+      print ('--> collision')
+      collision(20,0.8)
+  if (arg== "7"):
+      print ('--> collision')
+      collision(-20,0.8)
+      
 
 
-for x in range(3):
-  # Now for the algorith
-  moveWeel(speed)
-  sleep(1)
-  moveWeel(-speed)
-  sleep(1)
-  stopWeel()
+  
