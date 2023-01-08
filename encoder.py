d7c12041f39f2dbcb4cfd9cc7e7fc1321f6d837a
Dieter Tholen <dieter.tholen@web.de>
2023-01-08 14:00:21 +0100

    update

--- encoder.py
+++ encoder.py
@@ -1,10 +1,10 @@
 # Import modules
-import sys
+import sys, json
 from pitop import EncoderMotor, ForwardDirection  # Motor control library
 from time import sleep  # Allows us to tell the program to wait
 from pitop.miniscreen import Miniscreen
 from pitop.pma import UltrasonicSensor,Buzzer, Button, LED
-
+from tkinter import *
 
 ultrasonic_sensor = UltrasonicSensor("D0")
 
@@ -12,6 +12,7 @@ ultrasonic_sensor = UltrasonicSensor("D0")
 motor_r = EncoderMotor("M0", ForwardDirection.COUNTER_CLOCKWISE)
 motor_l = EncoderMotor("M1", ForwardDirection.COUNTER_CLOCKWISE)
 time_limit = 2
+scip_param = 0
 speed = 50
 counter = [0,0,0]
 setCounter = [100,100,100]
@@ -37,6 +38,11 @@ def collision(speed, time):
 def stopWeel():
   motor_r.stop()
   motor_l.stop()
+
+def button_save():
+    print("-->Speicher")
+def close():
+    fenster.destroy()
   
 args=sys.argv
 for arg in args:
@@ -65,7 +71,52 @@ for arg in args:
   if (arg== "7"):
       print ('--> collision')
       collision(-20,0.8)
-      
+  if (arg== "12"):
+      print ('--> move forward')
+      speed = int(input("Enter Speed : "))
+      duration = int(input("Enter duration (sec): "))
+      print("Speed:", speed)
+      moveWeel(int(speed))
+      sleep(duration)
+      stopWeel()
+  if (arg== "13"):
+      print ('--> move backward')
+      speed = int(input("Enter Speed : "))
+      duration = int(input("Enter duration (sec): "))
+      print("Speed:", speed)
+      moveWeel(int(speed))
+      sleep(duration)
+      stopWeel()
+  if (arg== "14"):
+      print ('--> turn r',speed)
+      speed = int(input("Enter Speed : "))
+      duration = int(input("Enter duration (sec): "))
+      print("Speed:", speed)
+      turn(speed,duration)
+  if (arg== "15"):
+      print ('--> turn l',speed)
+      speed = int(input("Enter Speed : "))
+      duration = int(input("Enter duration (sec): "))
+      print("Speed:", speed)
+      turn(-speed,duration)
+  if (arg== "20"):
+      print ('--> GUI')
+# some JSON:
+      data =  '{ "speed":"", "duration":1}'
+      fenster = Tk()
+      fenster.title("Define parameters")
+      fenster.title('PythonGuides')
+      fenster.geometry('400x300')
+      fenster.config(bg='#9FD996')
+      lMail = Label(fenster, text='Enter Email:', bg='#9FD996').grid(row=0, column=0,sticky = W, pady = 2,rowspan=2)
+      lPassword = Label(fenster, text='Enter Password:', bg='#9FD996').grid(row=1, column=0,sticky = W, pady = 2,rowspan=2)
+      eMail= Entry(fenster).grid(row=0, column=1)
+      ePassword=Entry(fenster).grid(row=1, column=1)
+      bCancel = Button(fenster,text='Cancel', command=close).grid(row=3, column=0)
+      bSave = Button(fenster,text='Login', command=button_save).grid(row=3, column=1)
+
+      fenster.mainloop()
+      print("-->finish")
 
 
   
